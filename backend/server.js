88ade5803981125c3633afe59f6e1c4224f43551
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-09-24 19:46:56 +0300

    feat: configure environment variables with dotenv

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,13 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet()); //helmet is a security middleware that helps you protect your app by setting various HTTP headers
@@ -14,6 +19,6 @@ app.get("/", (req, res) => {
   res.send("Hello from the backend");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
