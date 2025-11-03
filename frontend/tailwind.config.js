21ab0a00f10fd6324711d66689d1ca9a37468850
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-11-03 14:51:08 +0300

    chore: remove unused import from tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 import daisyui from "daisyui";
-import themes from "daisyui/theme/object";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
